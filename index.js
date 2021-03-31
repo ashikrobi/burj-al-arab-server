c1111d508d80e8513474110e2a4016d652dfd014
Ashiqur Rahman <ashikrobi@gmail.com>
2021-03-31 09:40:04 +0600

    fix config file exposed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.73puo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const port = 5000;
 
-var serviceAccount = require("burj-al-arab-7afbd-firebase-adminsdk-9ngt0-3cc452556a.json");
+var serviceAccount = require("./configs/burj-al-arab-7afbd-firebase-adminsdk-9ngt0-3cc452556a.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
